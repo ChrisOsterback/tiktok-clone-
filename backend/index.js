8730f01caabbfb096d1226a279c86d3d1484a8e0
Chris <Christian.Osterback@gmail.com>
2020-09-18 01:11:17 +0300

    tiktok clone
    all code Just have to fix some alignment issues

---
+++ backend/index.js
@@ -0,0 +1,69 @@
+// import
+import express from 'express'
+import mongoose from 'mongoose'
+import Cors from 'cors'
+
+
+import Videos from './dbModel.js'
+
+
+const api_mongo = require('./config.json');
+const token = api_mongo.token;
+// app config
+const app = express();
+const port = process.env.PORT || 9000
+
+
+// middleware
+app.use(express.json())
+app.use(Cors())
+
+
+// db config
+const connection_url = `${token}`
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+
+};
+
+mongoose.connect(connection_url, options)
+
+
+
+
+// api routes
+app.get("/", (req, res) => res.status(200).send("hello world"))
+
+app.get('/v2/posts', (req, res) => {
+    Videos.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+
+
+
+app.post("/v2/posts", (req, res) => {
+    const dbVideos = req.body
+
+
+    Videos.create(dbVideos, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(`new video created: \n ${data}`)
+        }
+    })
+
+})
+
+
+// listen
+app.listen(port, () => console.log(`Listening on port: ${port}`))
---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import "./css/App.css";
+
+import Video from "./components/Video";
+import axios from "./fetch/axios"
+
+function App() {
+
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get('/v2/posts')
+      setVideos(response.data)
+
+      return response;
+    }
+
+    fetchPosts();
+  }, [])
+
+
+
+  return (
+    <div className="app">
+      <div className="app_videos">
+        {videos.map(({ url, channel, description, song, likes, shares, messages }) => (
+          <Video
+            url={url}
+            channel={channel}
+            song={song}
+            likes={likes}
+            messages={messages}
+            description={description}
+            shares={shares}
+
+          />
+        ))}
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
